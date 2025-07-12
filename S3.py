6b1cc2295102b9165684af5036bb46142a55d397
Dis <dis-3-3@protonmail.com>
2025-07-12 15:25:29 +0300

    Update S3.py

--- S3.py
+++ S3.py
@@ -2,11 +2,11 @@ import json
 import boto3
 
 
-def get_client(access_key, secret_access_key, region_name):
+def get_client(access_key, secret_access_key):
     return boto3.client('s3',
         aws_access_key_id=access_key,
         aws_secret_access_key=secret_access_key,
-        region_name=region_name)
+        region_name='ru-1')
 
 
 def push_object(client, bucket, key, object):
