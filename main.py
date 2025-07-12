c791ae1ec1c69f8e40197c0b8f771fc7bccaf6f0
Dis <dis-3-3@protonmail.com>
2025-07-12 15:25:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import os
+import Secret
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, PlainTextResponse
 from DateTime import Now
 from index import index as index_page
 from S3 import get_client, push_object
 
-token = os.getenv('token')
-s3_bucket_name = os.getenv('s3_bucket_name')
-s3_access_key = os.getenv('s3_access_key')
-s3_secret_access_key = os.getenv('s3_secret_access_key')
 
-if token is None or '':
-    print('Не указан token')
-    exit()
+s3_bucket_name = Secret.get('s3_bucket_name')
+s3_access_key = Secret.get('s3_access_key')
+s3_secret_access_key = Secret.get('s3_secret_access_key')
 
 app = FastAPI()
+s3 = get_client(s3_access_key, s3_secret_access_key)
 
-s3 = get_client(
-    s3_access_key,
-    s3_secret_access_key,
-    'ru-1')
 
 @app.get("/", response_class=HTMLResponse)
 async def index():
@@ -52,6 +46,9 @@ def log_activity():
     error = push_object(s3, s3_bucket_name, now, {
         'activity': now
     })
+
+    if error:
+        raise Exception(error)
 
     with open('/home/.log', 'a', encoding='utf-8') as file:
         file.write(line)
