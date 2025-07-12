e6c72932094bb27ce20323f43d9379ba2bda71ee
Dis <dis-3-3@protonmail.com>
2025-07-12 19:19:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.responses import HTMLResponse, PlainTextResponse
 from DateTime import Now, Today
 from index import index as index_page
 from apscheduler.schedulers.background import BackgroundScheduler
+from VK import VK
 token = Secret.get('s3_bucket_name')
 
 s3 = S3(
