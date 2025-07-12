277d04dc1677448c691ca97e2b4c55545f2b01f9
Dis <dis-3-3@protonmail.com>
2025-07-12 13:05:49 +0300

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ index = '''
 <head>
     <meta charset="UTF-8">
     <title>Logs</title>
+    <script src="https://unpkg.com/react@18/umd/react.production.min.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js" crossorigin></script>
 </head>
 <body>
 
