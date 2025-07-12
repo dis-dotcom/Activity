4ac447e84519c53bf529409cc2aa2ce9a57b6dc6
Dis <dis-3-3@protonmail.com>
2025-07-12 11:58:14 +0300

    /log

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/", response_class=PlainTextResponse)
 async def root():
-    return "❤️"
+    return "❤"
+
+@app.get("/log")
+async def log():
+    return datetime.now()
