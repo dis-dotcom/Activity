6e18c3ac6b11f05382b7ad9a14cf9476e7f34417
Dis <dis-3-3@protonmail.com>
2025-07-12 15:33:46 +0300

    Update S3.py

--- S3.py
+++ S3.py
@@ -1,12 +1,16 @@
 import json
 import boto3
+from botocore.client import Config
 
 
 def get_client(access_key, secret_access_key):
     return boto3.client('s3',
+        endpoint_url='https://s3.twcstorage.ru',
         aws_access_key_id=access_key,
         aws_secret_access_key=secret_access_key,
-        region_name='ru-1')
+        region_name='ru-1',
+        config=Config(signature_version='s3v4')
+    )
 
 
 def push_object(client, bucket, key, object):
