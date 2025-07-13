fb8f44ea9c81712fe82ddc0a8cfce8a210d00c51
Dis <dis-3-3@protonmail.com>
2025-07-13 13:45:23 +0300

    Update LogActivityJob.py

--- LogActivityJob.py
+++ LogActivityJob.py
@@ -7,27 +7,26 @@ from DateTime import Now, Today, ToDateTime
 def run(vk: VK, s3: S3):
     try:
         print("Воркер запустился: " + Now())
-
-        for id in get('ids', ','):
-            log_activity(id, vk, s3)
-
+        [log_activity(x, vk, s3) for x in get('ids', ',')]
         print("Воркер завершился: " + Now())
     except Exception as ex:
         print("Воркер завершился с ошибкой: " + Now() + '\n' + str(ex))
 
 
-def log_activity(id: str, vk: VK, s3: S3):
-    user_info = vk.get_user_info(id)
+def log_activity(x: str, vk: VK, s3: S3):
+    user_info = vk.get_user_info(x)
 
-    if 'response' in user_info.keys() and type(user_info['response']) == type([]) and len(user_info['response']) > 0:
-        first = user_info['response'][0]
+    if 'response' not in user_info.keys(): return
+    if type(user_info['response']) != type([]): return
+    if len(user_info['response']) == 0: return
 
-        if 'last_seen' in first.keys():
-            user_info['last_activity'] = ToDateTime(first['last_seen']['time'], +3)
-        if 'online' in first.keys():
-            user_info['online'] = str(first['online']) == '1'
+    if 'last_seen' in user_info['response'][0].keys():
+        user_info['last_activity'] = ToDateTime(user_info['response'][0]['last_seen']['time'], +3)
 
-        s3.put_async(
-            key=f"{Today()}/{id}/{Now()}.json",
-            object=user_info
-        )
+    if 'online' in user_info['response'][0].keys():
+        user_info['online'] = str(user_info['response'][0]['online']) == '1'
+
+    s3.put_async(
+        key=f"{Today()}/{x}/{Now()}.json",
+        object=user_info
+    )
