63db75d5c7694d65d43a72ca03f6edcec01cf1b9
Dis <dis-3-3@protonmail.com>
2025-07-13 13:37:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import json
 import Secret
+import LogActivityJob
+
 from S3 import S3
 from VK import VK
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from DateTime import Now, Today, ToDateTime
 from index import index as index_page
 from apscheduler.schedulers.background import BackgroundScheduler
 
-
-ids = Secret.get('ids', ',')
 
 vk = VK(
     Secret.get('token')
@@ -23,7 +21,7 @@ s3 = S3(
 )
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(lambda: job(), 'interval', minutes=1)
+scheduler.add_job(lambda: LogActivityJob.run(vk, s3), 'interval', minutes=1)
 scheduler.start()
 
 app = FastAPI()
@@ -44,31 +42,4 @@ async def log():
     return {'objects': s3.get_objects('')[:10]}
 
 
-def job():
-    try:
-        print("Воркер запустился: " + Now())
-        log_activity()
-        print("Воркер завершился: " + Now())
-    except Exception as ex:
-        print("Воркер завершился с ошибкой: " + Now() + '\n' + str(ex))
-
-
-def log_activity():
-    for id in ids:
-        user_info = vk.get_user_info(id)
-
-        if 'response' in user_info.keys():
-            if len(user_info['response']) > 0:
-                first = user_info['response'][0]
-                if 'last_seen' in first.keys():
-                    user_info['last_activity'] = ToDateTime(first['last_seen']['time'], +3)
-                if 'online' in first.keys():
-                    user_info['online'] = str(first['online']) == '1'
-
-        s3.put_async(
-            key=f"{Today()}/{id}/{Now()}.json",
-            object=user_info
-        )
-
-
-log_activity()
+LogActivityJob.run(vk, s3)
