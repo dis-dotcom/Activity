37b139e19eacdb4eca9c5d53e1a6744ce9ed2daa
Dis <dis-3-3@protonmail.com>
2025-07-13 12:03:49 +0300

    Update DateTime.py

--- DateTime.py
+++ DateTime.py
@@ -3,15 +3,22 @@ from datetime import datetime, timezone, timedelta
 
 def Now():
     now = datetime.now(timezone.utc) + timedelta(hours=3)
-    milliseconds = now.microsecond // 1000
-    now_format = now.strftime("%Y-%m-%d %H:%M:%S") + f".{milliseconds:03d}"
-
-    return now_format
+    return formatDateTime(now)
 
 
 def Today():
     now = datetime.now(timezone.utc) + timedelta(hours=3)
-    now_format = now.strftime("%Y-%m-%d")
+    return formatDate(now)
 
-    return now_format
 
+def ToDateTime(ticks):
+    return formatDateTime(datetime.fromtimestamp(ticks))
+
+
+def formatDateTime(x):
+    milliseconds = x.microsecond // 1000
+    return x.strftime("%Y-%m-%d %H:%M:%S") + f".{milliseconds:03d}"
+
+
+def formatDate(x):
+    return x.strftime("%Y-%m-%d")
