09a62af3c9a07b2ba0b0669d1ec21e3779082311
Dis <dis-3-3@protonmail.com>
2025-07-13 13:06:43 +0300

    Update Secret.py

--- Secret.py
+++ Secret.py
@@ -1,10 +1,18 @@
 import os
 
 
-def get(arg_name):
-    arg = os.getenv(arg_name)
+isLocal = False
+}
+
+def get(arg_name, sep=None):
+    arg = secrets[arg_name] if isLocal else os.getenv(arg_name)
 
     if arg is None or '':
         raise Exception('Отсутствует параметр ' + arg_name)
 
+    if sep:
+        arg = arg.split(sep)
+
     return arg
+
+
