ffe0b4a7819cce1154391610f5ece258963f9142
Dis <dis-3-3@protonmail.com>
2025-07-13 17:42:29 +0300

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -2,3 +2,8 @@ class Logger:
     @staticmethod
     def info(message: str):
         print(f'INFO:\t{message}')
+
+    @staticmethod
+    def error(message: str, ex: Exception=None):
+        ex_str = '' if ex is None else '\n' + str(ex)
+        print(f'ERROR:\t{message}{ex_str}')
