b9ca8f073a0744b1bbd7b42eee5fa1f9d2a006f9
Dis <dis-3-3@protonmail.com>
2025-09-29 01:05:56 +0300

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -1,9 +1,18 @@
 class Logger:
+    logs = []
+
     @staticmethod
     def info(message: str):
-        print(f'INFO:\t{message}')
+        Logger.log(f'INFO:\t{message}')
 
     @staticmethod
     def error(message: str, ex: Exception=None):
         ex_str = '' if ex is None else '\n' + str(ex)
-        print(f'ERROR:\t{message}{ex_str}')
+        Logger.log(f'ERROR:\t{message}{ex_str}')
+
+    @staticmethod
+    def log(message: str):
+        if len(Logger.logs) > 10000:
+            Logger.logs = Logger.logs[100:]
+
+        Logger.logs.append(message)
